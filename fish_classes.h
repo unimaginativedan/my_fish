1de82e824ca6c73477e1a3f2860db334359e922c
Daniel Purdy <purdyd@chuck.tchpc.tcd.ie>
2015-03-23 20:31:50 +0000

    Move in class definition, all working

--- fish_classes.h
+++ fish_classes.h
@@ -3,47 +3,121 @@
 using namespace std;
 
 class fish {
+  // Base class that defines all fish.
  public:
   int x, y, z;
   int age, eaten;
-
+    
   void init(){
     // Function to initialise fish position, age and status.
     age = 0;
     eaten=0;
-
-    x=rand()%25;
-    y=rand()%25;
-    z=rand()%25;
+   
+    x=rand()%5;
+    y=rand()%5;
+    z=rand()%5;
   }
+  
+  void position_plusminus( int &k){
+    // Used in many move functions useful to have.
+    if(rand()%2)
+      k=(k+1)%5;
+    else
+      k=(k+4)%5;
+  }
+  
 };
 
 class minnow: public fish{
+  // Derived class that defines minnows, specifically how they move.
  public:
   void move(){
-    x+=((rand()%3)-1)%25;
-    y+=((rand()%3)-1)%25;
-    z+=((rand()%3)-1)%25;
+    x=(x+((rand()%3)-1))%5;
+    y=(y+((rand()%3)-1))%5;
+    z=(z+((rand()%3)-1))%5;
   }
-
 };
 
 class tuna: public fish{
  public:
+  // Derived class that defines tuna, specifically how they move.
   void move(){
+   
+    // Choose which diagonal plane to move in.
     int diag;
     diag=rand()%3;
-    if(diag==0) {
-      // Move in xy plane.
-      
-    }
-    else if(diag==1) {
-      // Move in xz plane.
-    }
-    else {
-      // Move in yz plane.
-     
+    switch (diag){
+    case 0:
+      plane_move(x,y);
+      break;
+    case 1:
+      plane_move(x,z);
+      break;
+    case 2:
+      plane_move(y,z);
+      break;
+    default :
+      cout << "Error choosing plane"<< endl;
+    }    
+  }
+  
+  void plane_move(int &i, int &j){
+    // Given a plane choose a diagonal.
+    position_plusminus(i);
+    position_plusminus(j);
+  }
+
+
+};
+
+class shark: public fish{
+ public:
+  // Derived class that defines sharks, specifically how they move.
+  void move(){
+   
+    // Choose which plane to move in.
+    int diag;
+    diag=rand()%3;
+    switch (diag){
+    case 0:
+      plane_move(x,y);
+      break;
+    case 1:
+      plane_move(x,z);
+      break;
+    case 2:
+      plane_move(y,z);
+      break;
+    default :
+      cout << "Error choosing plane"<< endl;
+    }    
+  }
+  
+  void plane_move(int &i, int &j){
+    // Given a plane choose a diagonal.
+    int dir=rand()%4;
+    switch(dir){
+    case 0:
+      i=(i+2)%5;
+      position_plusminus(j);
+      break;
+    case 1:
+      j=(j+2)%5;
+      position_plusminus(i);
+      break;
+    case 2:
+      j=(j-2)%5;
+      position_plusminus(i);
+      break;
+    case 3:
+      i=(i-2)%5;
+      position_plusminus(j);
+      break;
+    default:
+      cout << "Error moving shark"<< endl;
     }
   }
+
+  
 
 };
--- fish_tank.cc
+++ fish_tank.cc
@@ -7,17 +7,18 @@ using namespace std;
 int main() {
   
   srand(time(NULL));
-  minnow m1;
-  m1.init();
-  cout<<"Init minnow position: "<< m1.x << " " << m1.y << "  " << m1.z << endl;
+  shark s1;
+  s1.init();
+  cout<<"Init shark position: "<< s1.x << " " << s1.y << "  " << s1.z << endl;
 
-  m1.move();
-  cout<<"Next minnow position: "<< m1.x << " " << m1.y << "  " << m1.z << endl;
+  s1.move();
+  cout<<"Next shark position: "<< s1.x << " " << s1.y << "  " << s1.z << endl;
+  
+  s1.move();  
+  cout<<"Next shark position: "<< s1.x << " " << s1.y << "  " << s1.z << endl;
+  
+  s1.move();
+  cout<<"Finl shark position: "<< s1.x << " " << s1.y << "  " << s1.z << endl;
 
-  m1.move();
-  cout<<"Next minnow position: "<< m1.x << " " << m1.y << "  " << m1.z << endl;
-
-
-  m1.move();
-  cout<<"Finl minnow position: "<< m1.x << " " << m1.y << "  " << m1.z << endl;
+  
 }
